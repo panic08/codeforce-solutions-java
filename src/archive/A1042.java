0f3960bebfd32fffa2432505a5d5960bf17d742c
Andrey <andrey1010102008@gmail.com>
2024-10-14 17:29:55 +0300

    first commit

---
+++ src/archive/A1042.java
@@ -0,0 +1,46 @@
+package archive;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class A1042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int max = 0;
+
+        PriorityQueue<Integer> nums = new PriorityQueue<>();
+
+        int it = 0;
+
+        while (it < n) {
+            int num = sc.nextInt();
+
+            nums.add(num);
+
+            max = Math.max(num, max);
+
+            it++;
+        }
+
+        max += m;
+
+        for (int i = 0; i < m; i++) {
+            int prev = nums.poll();
+            prev++;
+
+            nums.add(prev);
+        }
+
+        int val = 1;
+
+        while (!nums.isEmpty()) {
+            val = nums.poll();
+        }
+
+        System.out.println(val + " " + max);
+    }
+}
